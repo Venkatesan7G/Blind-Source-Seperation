811ccfc9236fa260d1d7debbd3da3d9488442338
Your Name <you@example.com>
2026-01-06 18:22:23 +0100

    Clean slate

--- extras/check_amp.py
+++
@@ -1,6 +0,0 @@
-import soundfile as sf
-import numpy as np
-from audiomentations import AddGaussianNoise
-
-my_waveform_ndarray, sample_rate = sf.read("speech_recordings/speechcn.wav")
-print(f"Original sample rate: {sample_rate} Hz")
